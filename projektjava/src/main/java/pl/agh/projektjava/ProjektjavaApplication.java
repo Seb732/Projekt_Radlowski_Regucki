098a602cdca36b8f5e91d5c7e02db43e77489653
JRadlowski <79854117+JRadlowski@users.noreply.github.com>
2022-03-23 19:10:39 +0100

    setup

---
+++ projektjava/src/main/java/pl/agh/projektjava/ProjektjavaApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.projektjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektjavaApplication.class, args);
+	}
+
+}
---
+++ projektjava/src/test/java/pl/agh/projektjava/ProjektjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.projektjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
