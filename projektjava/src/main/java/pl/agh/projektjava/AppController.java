9c5a5fa28538ad8badd9ea1a05df6bc73700a1c4
JRadlowski <jradlowski@student.agh.edu.pl>
2022-03-23 20:52:07 +0100

    setup

---
+++ projektjava/src/main/java/pl/agh/projektjava/AppController.java
@@ -0,0 +1,15 @@
+package pl.agh.projektjava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String hello()
+    {
+        return "hello";
+    }
+    
+}
